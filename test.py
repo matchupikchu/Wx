7ecd5bad0c58e80508b225725879ab7270155a64
Mateusz Chudzik <matchupikchu99@gmail.com>
2023-02-27 17:46:08 +0100

    push

--- test.py
+++ test.py
@@ -3,42 +3,57 @@ import random
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer
-
-from utilities import InputDriver, IO_Monitor, Wx
+from cocotb.regression import TestFactory
+from utilities import InputDriver, IO_Monitor, Polynomial, Wx
    
 
-@cocotb.test()
-async def test_case_classes(dut):
+# @cocotb.test()
+# async def test_case_classes(dut):
 
-    clock = Clock(dut.in_clock, 10, units="ns")
-    await cocotb.start(clock.start())
+#     clock = Clock(dut.in_clock, 10, units="ns")
+#     cocotb.start(clock.start())
 
-    global expected_value
-    expected_value = []
+#     global expected_value
+#     expected_value = []
 
-    dut.axis_s_tvalid.value = 0
-    dut.axis_m_tready.value = 1
-    dut.axis_s_tdata.value = 0
+#     dut.axis_s_tvalid.value = 0
+#     dut.axis_m_tready.value = 1
+#     dut.axis_s_tdata.value = 0
 
-    await Timer(5, "ns")
 
-    in_driver = InputDriver(dut, "axis_s", dut.in_clock)
-    slave_monitor = IO_Monitor(dut, name = "axis_s", clock = dut.in_clock)
-    master_monitor = IO_Monitor(dut, name = "axis_m", clock = dut.in_clock, callback=Wx)
+#     in_driver = InputDriver(dut, "axis_s", dut.in_clock)
+#     slave_monitor = IO_Monitor(dut, name = "axis_s", clock = dut.in_clock)
+#     master_monitor = IO_Monitor(dut, name = "axis_m", clock = dut.in_clock, callback=Wx)
+
+#     for i in range(10):
+
+#         x = random.randint(0, 200)
+        
+#         if i == 0:
+#             x = 0
+
+#         expected_value.append(Wx(x))
+        
+#         await in_driver._driver_send(x)
+
+
+async def test(dut):
+    
+    tb = Polynomial(dut)
+
+    tb.start_clock()
 
     for i in range(10):
-
         x = random.randint(0, 200)
         
         if i == 0:
             x = 0
 
-        expected_value.append(Wx(x))
-        
-        await in_driver._driver_send(x)
+        await tb.axis_s_driver._driver_send(x)
 
 
 
 
-        
+factory = TestFactory(test) 
+factory.generate_tests()
 
--- utilities.py
+++ utilities.py
@@ -47,15 +47,39 @@ class IO_Monitor(BusMonitor):
         self._signals = signals
         BusMonitor.__init__(self, entity, name, clock, bus_separator = bus_separator, callback = None)
         self.clock = clock
-        self.axis_m_tvalid = getattr(self.bus,list(filter(lambda x: 'tvalid' in x, self._signals))[0])
-        self.axis_m_tready = getattr(self.bus,list(filter(lambda x: 'tready' in x, self._signals))[0])
-        self.axis_m_tdata  = getattr(self.bus,list(filter(lambda x: 'tdata' in x, self._signals))[0])
+        self.tvalid = getattr(self.bus,list(filter(lambda x: 'tvalid' in x, self._signals))[0])
+        self.tready = getattr(self.bus,list(filter(lambda x: 'tready' in x, self._signals))[0])
+        self.tdata  = getattr(self.bus,list(filter(lambda x: 'tdata' in x, self._signals))[0])
 
         
     @cocotb.coroutine
     def _monitor_recv(self):
         while True:
             yield RisingEdge(self.clock)
+            if self.tvalid.value == 1:
+                self.log.info(f"{self.name} tvalid {int(self.tvalid)}")
+                self.log.info(f"{self.name} tready {int(self.tready)}")
+                self.log.info(f"{self.name} tdata {int(self.tdata)}")
 
-            if self.axis_m_tvalid.value:
-                self.log.info(f"Received W(x) = {int(self.axis_m_tdata)}")
+
+# class Polynomianl()
+class TbPolynomial(object):
+    def __init__(self, dut):
+        self.dut = dut
+    
+    def start_clock(self, clk_period = 10):
+        self.dut._log.info("Running clock")
+        cocotb.start_soon(Clock(self.dut.in_clock, clk_period,units='ns').start())
+    
+
+class Polynomial(TbPolynomial):
+    def __init__(self, dut):
+        super(Polynomial, self).__init__(dut)
+
+        self.dut.axis_s_tvalid.value = 0
+        self.dut.axis_m_tready.value = 1
+        self.dut.axis_s_tdata.value = 0
+
+        self.axis_s_driver = InputDriver(dut, "axis_s", dut.in_clock)
+        self.axis_s_monitor = IO_Monitor(dut, name = "axis_s", clock = dut.in_clock)
+        self.axis_m_monitor = IO_Monitor(dut, name = "axis_m", clock = dut.in_clock, callback=Wx)
