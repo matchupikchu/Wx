a11f509ebef11ff1b0fc60ea469d639997289557
Mateusz Chudzik <matchupikchu99@gmail.com>
2023-02-28 11:08:23 +0100

    dsadas

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import random
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer
-# from cocotb.scoreboard import Scoreboard
-# from cocotb.scoreboard import Scoreboard
+from cocotb.scoreboard import Scoreboard
 from cocotb.regression import TestFactory
 from utilities import InputDriver, IO_Monitor, Polynomial, Wx
 
@@ -16,48 +15,45 @@ async def test_case_classes(dut):
     clock = Clock(dut.in_clock, 10, units="ns")
     cocotb.start(clock.start())
 
-    global expected_value
-    # # expected_value = []
+    # global expected_value
+    # expected_value = []
 
     dut.axis_s_tvalid.value = 0
     dut.axis_m_tready.value = 1
     dut.axis_s_tdata.value = 0
 
 
-#     in_driver = InputDriver(dut, "axis_s", dut.in_clock)
-#     slave_monitor = IO_Monitor(dut, name = "axis_s", clock = dut.in_clock)
-#     master_monitor = IO_Monitor(dut, name = "axis_m", clock = dut.in_clock, callback=Wx)
+    in_driver = InputDriver(dut, "axis_s", dut.in_clock)
+    slave_monitor = IO_Monitor(dut, name = "axis_s", clock = dut.in_clock)
+    master_monitor = IO_Monitor(dut, name = "axis_m", clock = dut.in_clock, callback=Wx)
 
     await in_driver._driver_send(0)
 
-#         x = random.randint(0, 200)
-        
-#         if i == 0:
-#             x = 0
+    for _ in range(10):
 
-#         expected_value.append(Wx(x))
+        x = random.randint(0, 2*16)
+
+        # expected_value.append(Wx(x))
         
         await in_driver._driver_send(x)
 
-@cocotb.test()
-def test(dut):
+
+async def test(dut):
     
     tb = Polynomial(dut)
-    dut.axis_m_tready.value = 1
-
     dut.axis_m_tready.value = 1
 
 
     tb.start_clock()
 
-    for i in range(10):
+    for _ in range(10):
         x = random.randint(0, 2**16)
 
-        yield tb.axis_s_driver._driver_send(x)
+        await tb.axis_s_driver._driver_send(x)
 
 
 
 
-# factory = TestFactory(test) 
-# factory.generate_tests()
+factory = TestFactory(test) 
+factory.generate_tests()
 
